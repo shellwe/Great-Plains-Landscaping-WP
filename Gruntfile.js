aa91e7a95fa0c3546e6b39a6833b260419c05bf9
shellwe <shellwe@gmail.com>
2015-03-18 21:43:36 -0500

    played with grunt some more

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,17 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
+	    autoprefixer: {
+    options: {
+		  browsers: ['last 2 versions', 'ie 8', 'ie 9']
+
+      // Task-specific options go here.
+    },
+    your_target: {
+      // Target-specific file lists and/or options go here.
+    },
+  },
+
     pkg: grunt.file.readJSON('package.json'),
 	  sass: {                              // Task
     dist: {                            // Target
@@ -24,7 +35,7 @@ module.exports = function(grunt) {
       }
     }
   });
-
+  grunt.loadNpmTasks('grunt-autoprefixer');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-uglify');

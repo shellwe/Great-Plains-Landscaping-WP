516fa2c4e1a555535866b42526d82dfdfec7f982
shellwe <shellwe@gmail.com>
2015-03-29 04:03:01 -0500

    more changes apparently

--- functions.php
+++ functions.php
@@ -109,8 +109,10 @@ function gpl_scripts() {
 	//skip-link-focus-fix has to do with going to a special /#content section designed for screen readers, if I delete this out of the head I shoudl comment this out.
 	wp_enqueue_script( 'gpl-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
+/*
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
+*/
 	}
 }
 add_action( 'wp_enqueue_scripts', 'gpl_scripts' );

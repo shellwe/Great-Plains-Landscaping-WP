8f0063f3dd98555a0b0b7cf8f89f24a39c6f0a7a
shellwe <shellwe@gmail.com>
2015-04-21 00:20:18 -0500

    forgot to include header
    Adding header now

--- header.php
+++ header.php
@@ -31,7 +31,6 @@
 
 
       <nav class="navbar navbar-default">
-        <div class="container-fluid">
           <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
               <span class="sr-only">Toggle navigation</span>
@@ -39,7 +38,7 @@
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">Project name</a>
+            <a class="navbar-brand hidden-md hidden-lg" href="#">Great Plains Landscaping</a>
           </div>
           <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
@@ -65,13 +64,12 @@
               <li><a href="../navbar-fixed-top/">Fixed top</a></li>
             </ul>
           </div><!--/.nav-collapse -->
-        </div><!--/.container-fluid -->
       </nav>
 
 
-		<nav id="site-navigation" class="site-navigation menu col-md-12 col-sm-6" role="navigation">
-			<button class="menu-toggle" aria-controls="menu" aria-expanded="false"><?php _e( 'Primary Menu', 'gpl' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-		</nav>
+<!--		<nav id="site-navigation" class="site-navigation menu col-md-12 col-sm-6" role="navigation">
+			<button class="menu-toggle" aria-controls="menu" aria-expanded="false"><?php // _e( 'Primary Menu', 'gpl' ); ?></button>
+			<?php // wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+		</nav>-->
     </div>
 </header>

cea51b318fc0544b201d311dc9c7d4fcde41a4ba
Chet G <chetgassett@gmail.com>
2015-04-28 09:11:48 -0500

    commit the weird deleted files

--- functions.php
+++ functions.php
@@ -139,9 +139,6 @@ require get_template_directory() . '/inc/extras.php';
  */
 require get_template_directory() . '/inc/customizer.php';
 
-require_once('wp_bootstrap_navwalker.php');
-
-
 /**
  * Load Jetpack compatibility file.
  */

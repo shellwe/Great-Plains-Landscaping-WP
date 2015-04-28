213795f8d1db76888e6f3a1b18faff714b7d2672
shellwe <shellwe@gmail.com>
2015-04-28 17:31:42 -0500

    putting back files Chet undid
    Distracted I told Chet it was okay to roll back this weekend's stuff
    when I needed it, so hopefully it is all there now.

--- footer.php
+++ footer.php
@@ -25,7 +25,7 @@
                     </address>
                 </div>
                 <div class="col-md-4 col-sm-6">
-					this is where the facebook feed goes
+					<?php [custom-facebook-feed] ?>
                 </div>
                 <div class="col-md-4 col-sm-12">
                     <form>
--- header.php
+++ header.php
@@ -18,6 +18,16 @@
 <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-18166885-5', 'auto');
+  ga('send', 'pageview');
+
+</script>
 <div id="page" class="hfeed site">
 <a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'gpl' ); ?></a>
 <header class="container-fluid document-header">
@@ -27,7 +37,6 @@
 				<img src="<?php bloginfo('template_directory'); ?>/source/puresite/img/logo.png" width="369" height="102" alt=""/>
 			</div>
 		</div>
-<<<<<<< HEAD
 		<nav class="navbar navbar-default" role="navigation"> 
 		<!-- Brand and toggle get grouped for better mobile display --> 
 		  <div class="navbar-header"> 
@@ -52,60 +61,6 @@
 			);
 			?>
 		  </div>
-=======
-
-
-
-      <nav class="navbar navbar-default">
-        <div class="container-fluid">
-          <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
-<<<<<<< HEAD
-            <a class="navbar-brand" href="#">Project name</a>
-=======
-            <a class="navbar-brand hidden-md hidden-lg" href="#">Great Plains Landscaping</a>
->>>>>>> parent of d82dc15... making progress with wordpress nav
-          </div>
-          <div id="navbar" class="navbar-collapse collapse">
-            <ul class="nav navbar-nav">
-              <li class="active"><a href="#">Home</a></li>
-              <li><a href="#">About</a></li>
-              <li><a href="#">Contact</a></li>
-              <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                <ul class="dropdown-menu" role="menu">
-                  <li><a href="#">Action</a></li>
-                  <li><a href="#">Another action</a></li>
-                  <li><a href="#">Something else here</a></li>
-                  <li class="divider"></li>
-                  <li class="dropdown-header">Nav header</li>
-                  <li><a href="#">Separated link</a></li>
-                  <li><a href="#">One more separated link</a></li>
-                </ul>
-              </li>
-            </ul>
-            <ul class="nav navbar-nav navbar-right">
-              <li class="active"><a href="./">Default <span class="sr-only">(current)</span></a></li>
-              <li><a href="../navbar-static-top/">Static top</a></li>
-              <li><a href="../navbar-fixed-top/">Fixed top</a></li>
-            </ul>
-          </div><!--/.nav-collapse -->
-        </div><!--/.container-fluid -->
-      </nav>
-
-
-		<nav id="site-navigation" class="site-navigation menu col-md-12 col-sm-6" role="navigation">
-			<button class="menu-toggle" aria-controls="menu" aria-expanded="false"><?php _e( 'Primary Menu', 'gpl' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-<<<<<<< HEAD
->>>>>>> parent of 8f0063f... forgot to include header
-=======
->>>>>>> parent of 8f0063f... forgot to include header
 		</nav>
     </div>
 </header>

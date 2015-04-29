2c812a049cba78abd18bdcd3c2da482095158a6f
shellwe <shellwe@gmail.com>
2015-04-29 00:02:00 -0500

    code to handle galleries inserted
    threw in code to handle the galleries, it just displays a list but I can
    manipulate it as needed.
    https://codex.wordpress.org/Function_Reference/wp_get_attachment_image

--- front-page.php
+++ front-page.php
@@ -20,6 +20,30 @@
 			
 			</div>
 		</div>
+        <ul>
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post();    
+
+ $args = array(
+   'post_type' => 'attachment',
+   'numberposts' => -1,
+   'post_status' => null,
+   'post_parent' => $post->ID
+  );
+
+  $attachments = get_posts( $args );
+     if ( $attachments ) {
+        foreach ( $attachments as $attachment ) {
+           echo '<li>';
+           echo wp_get_attachment_image( $attachment->ID, 'full' );
+           echo '<p>';
+           echo apply_filters( 'the_title', $attachment->post_title );
+           echo '</p></li>';
+          }
+     }
+
+ endwhile; endif; ?>
+</ul>
+
 	</main>
 </div>
 <?php get_footer(); ?>

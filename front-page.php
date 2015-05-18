cb1baa3e1b8681f59b060cf2c174788b5b9026b5
shellwe <shellwe@gmail.com>
2015-05-17 23:47:16 -0500

    changed front page
    added second video... sigh...

--- front-page.php
+++ front-page.php
@@ -5,6 +5,11 @@
             <?php wp_get_attachment_image( 4, large, false, $default_attr ); ?>
         </div>
 	</div>
+	<div class="row">
+		<div class="col-md-12">
+            <p>Great Plains Landscaping &amp; Design delivers unparalleled craftsmanship in landscape design, construction and maintenance at reasonable prices. We are dedicated to provide our customer with prompt and professional customer service. Our team believes in a strong client relationship built on working together, trust and understanding the desired outdoor spaces.</p>
+        </div>
+	</div>
 	<main class="row">
 		<div class="col-md-4">
 			<div class="embed-responsive embed-responsive-16by9">
@@ -15,9 +20,8 @@
 			<div class="banner">We now specialize in Epoxy protective flooring!</div>
 		</div>
 		<div class="col-md-4 col-sm-6">
-			<div class="banner">
-			<h5>How to Connect</h5>
-			
+			<div class="embed-responsive embed-responsive-16by9">
+				<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/lEp5e9L7wi0"></iframe>
 			</div>
 		</div>
         <ul>

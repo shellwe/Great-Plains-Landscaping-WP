594b3774674e07cc51590fc56571b623a1ba3cc7
shellwe <shellwe@gmail.com>
2015-05-23 22:59:16 -0500

    changed breakpoints with for columns
    Change for front page.

--- front-page.php
+++ front-page.php
@@ -11,7 +11,7 @@
         </div>
 	</div>
 	<div class="row">
-		<div class="col-md-4">
+		<div class="col-md-4 col-sm-6">
 			<div class="embed-responsive embed-responsive-16by9">
 				<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/vl0nmm1aVtA"></iframe>
 			</div>
@@ -19,7 +19,7 @@
 		<div class="col-md-4 col-sm-6">
 			<div class="banner">We now specialize in Epoxy protective flooring!</div>
 		</div>
-		<div class="col-md-4 col-sm-6">
+		<div class="col-md-4 col-sm-12">
 			<div class="embed-responsive embed-responsive-16by9">
 				<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/lEp5e9L7wi0"></iframe>
 			</div>

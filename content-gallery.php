6fcaa92a75c828f7bbf8fd8716a03c7f6e83c4a5
shellwe <shellwe@gmail.com>
2015-07-05 23:42:25 -0500

    made custom gallery page
    removed superfluous header and footer content

--- content-gallery.php
+++ content-gallery.php
@@ -7,23 +7,12 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
-
-		<div class="entry-meta">
-			<?php gpl_posted_on(); ?>
-		</div><!-- .entry-meta -->
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
 		<?php the_content(); ?>
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . __( 'Pages:', 'gpl' ),
-				'after'  => '</div>',
-			) );
-		?>
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
-		<?php gpl_entry_footer(); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
--- single.php
+++ single.php
@@ -1,16 +1,12 @@
-<?php get_header();
-$isStandardCustom = has_post_format( 'Standard' );
-$isGalleryCustom = has_post_format( 'Gallery' );
-?>
+<?php get_header();?>
 <div id="primary single-layout" class="content-area page container">
     <div class="row">
 		<main id="main" class="site-main <?php if (has_post_format( 'Standard' )) {echo "col-md-9 col-sm-8"; } else {echo "col-md-12";}?> " role="main">
 
 		<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php get_template_part( 'content', 'single' ); ?>
-
-			<?php gpl_post_nav(); ?>
+			<?php if (has_post_format( 'Standard' )) {get_template_part( 'content', 'single' );}
+			if (has_post_format( 'Gallery' )) {get_template_part( 'content', 'gallery' );}?>
 
 		<?php endwhile; // end of the loop. ?>
 

06de4ebfc1abfddf498f94473da22d9f36a006b0
shellwe <shellwe@gmail.com>
2015-08-27 01:58:43 -0500

    fixed issue with archive page not showing
    Performed loop poorly, got help from reddit.

--- single.php
+++ single.php
@@ -4,9 +4,13 @@
 		<main id="main" class="site-main <?php if (has_post_format( 'Standard' )) {echo "col-md-9 col-sm-8"; } else {echo "col-md-12";}?> " role="main">
 
 		<?php while ( have_posts() ) : the_post(); ?>
-
-			<?php if (has_post_format( 'Standard' )) {get_template_part( 'content', 'single' );}
-			if (has_post_format( 'Gallery' )) {get_template_part( 'content', 'gallery' );}?>
+<?php
+    if ( has_post_format( 'gallery' ) ) {
+        get_template_part( 'content', 'gallery' );
+    } else {
+        get_template_part( 'content', 'single' );
+    }
+?>
 
 		<?php endwhile; // end of the loop. ?>
 

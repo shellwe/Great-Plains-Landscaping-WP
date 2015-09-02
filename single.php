cecc4d628b7e59f2b08d08913896a951b4a6b3da
shellwe <shellwe@gmail.com>
2015-09-02 17:58:20 -0500

    changed to setup some proper coding via a suggestion on reddit
    Need to make note of this in case I need to roll back.

--- single.php
+++ single.php
@@ -1,7 +1,8 @@
 <?php get_header();?>
 <div id="primary single-layout" class="content-area page container">
     <div class="row">
-		<main id="main" class="site-main <?php if (has_post_format( 'Standard' )) {echo "col-md-9 col-sm-8"; } else {echo "col-md-12";}?> " role="main">
+		<main id="main" class="site-main <?php
+        if( get_post_format() === false ) {echo "col-md-9 col-sm-8";} else {echo "col-md-12";}?> " role="main">
 
 		<?php while ( have_posts() ) : the_post(); ?>
 <?php
